3e45f8183be2b6f44fddb628d840099ab0dece6a
Diego Lima <xxdiegolsxx@gmail.com>
2022-12-24 19:23:43 -0300

    feat: product page

---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductsService } from 'src/app/services/products.service';
+import { IProducts } from 'src/app/types/products';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss'],
+})
+export class ProductsComponent {
+  products: IProducts[] | undefined;
+
+  constructor(
+    private productsService: ProductsService,
+    private router: Router
+  ) {
+    this.products = this.productsService.getAll();
+  }
+  goToDetails = (id: number) => {
+    return this.router.navigate([`products/${id}`]);
+  };
+}
