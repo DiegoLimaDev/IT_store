cbb245d087a891e532b76c26b10e52cc27c3ac0f
Diego Lima <xxdiegolsxx@gmail.com>
2022-12-25 16:56:55 -0300

    feat: notification service

---
+++ src/app/services/notificiation/notification.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NotificationService {
+  constructor(private snackBar: MatSnackBar) {}
+
+  notified = (msg: string) => {
+    this.snackBar.open(msg, 'OK', {
+      duration: 2000,
+      verticalPosition: 'top',
+      horizontalPosition: 'center',
+    });
+  };
+}
