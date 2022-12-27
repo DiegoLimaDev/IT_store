f4199b68cb4025b055a1cc5f542ad0e7a10b3ff4
Diego Lima <xxdiegolsxx@gmail.com>
2022-12-26 22:13:45 -0300

    feat: added search bar and configured filter

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductsService } from 'src/app/services/products/products.service';
 import { IProducts } from 'src/app/types/products';
@@ -8,16 +8,30 @@ import { IProducts } from 'src/app/types/products';
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss'],
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
   products: IProducts[] | undefined;
+  search = '';
 
   constructor(
     private productsService: ProductsService,
     private router: Router
-  ) {
+  ) {}
+
+  ngOnInit() {
     this.products = this.productsService.getAll();
   }
   goToDetails = (id: number) => {
     return this.router.navigate([`products/${id}`]);
+  };
+
+  filteringSearch = (name: string) => {
+    if (this.search != '') {
+      this.products = this.productsService.getAll();
+      this.products = this.products?.filter((e) =>
+        e.description.toLowerCase().match(name.toLowerCase())
+      );
+    } else {
+      this.products = this.productsService.getAll();
+    }
   };
 }
--- src/app/components/products/products.module.ts
+++ src/app/components/products/products.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products.component';
 import { SharedModule } from 'src/app/shared.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ProductsComponent],
-  imports: [CommonModule, ProductsRoutingModule, SharedModule],
+  imports: [CommonModule, ProductsRoutingModule, SharedModule, FormsModule],
 })
 export class ProductsModule {}
