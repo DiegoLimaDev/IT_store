a3541200bcc4ea4fef829d9cd7ad7dc475d94368
Diego Lima <xxdiegolsxx@gmail.com>
2023-01-05 13:07:24 -0300

    refactor: routing

--- src/app/components/products/products-routing.module.ts
+++ src/app/components/products/products-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './products.component';
 
-const routes: Routes = [{ path: 'products', component: ProductsComponent }];
+const routes: Routes = [{ path: '', component: ProductsComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
